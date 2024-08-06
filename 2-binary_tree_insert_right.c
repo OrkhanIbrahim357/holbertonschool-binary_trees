ae5667d11b82f899d4b6a5df655655bf824c4128
sadatnazarli <sedat.nazarli@gmail.com>
2024-08-06 11:44:44 +0000

    problem fixed

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -17,17 +17,17 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 
-	new_mode = binary_tree_node(parent, value);
-	if (new_mode == NULL)
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
 	{
 		return (NULL);
 	}
 	new_node->right = parent->right;
 	if (parent->right != NULL)
 	{
-		parent->right->parent = new_mode;
+		parent->right->parent = new_node;
 	}
-	parent->right = new_mode;
+	parent->right = new_node;
 
-	return (new_mode);
+	return (new_node);
 }
