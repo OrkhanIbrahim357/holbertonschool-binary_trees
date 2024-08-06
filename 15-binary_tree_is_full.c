8b7ba9c09487a1e4de324406cd0f7a51f24a6ae7
Orkhanibrahim357 <9418@holbertonstudents.com>
2024-08-06 14:52:46 +0000

    betty found bug and bug cleaning task 15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,22 +1,20 @@
 #include "binary_trees.h"
 /**
- * binary_tree_is_full - function that checks if a binary tree is full
- *
- * @tree: Pointer to the root node of the tree.
- *
- * Return: if node is full 1, otherwise 0.
+ *binary_tree_is_full - checks if a binary tree is full
+ *@tree: the root node of the tree
+ *Return: 1 or 0
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (!tree)
+	if (tree == NULL)
 		return (0);
+
 	if (tree->left == NULL && tree->right == NULL)
-	{
 		return (1);
-}
-	if (tree->left == NULL || tree->right == NULL)
-		{
-			return (0);
-		}
-	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	if (tree->left && tree->right)
+	{
+		return (binary_tree_is_full(tree->left) &&
+			binary_tree_is_full(tree->right));
+	}
+	return (0);
 }
