a6c66996652bc712d866e8283f835057b90e7c8e
Orkhanibrahim357 <orkhan_ibrahim@mail.ru>
2024-08-06 12:34:38 +0000

    adding new prototype

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+{
+	binary_tree_t = *new_node;
+	if (new_node == NULL)
+	{
+		return(NULL);
+	}
+	new_node = (malloc(sizeof(binary_tree_t);
+
