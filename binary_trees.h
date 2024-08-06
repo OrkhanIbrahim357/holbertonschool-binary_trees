1bf33be65c135b98ce88651855b8ba88fd1d9392
Orkhanibrahim357 <orkhan_ibrahim@mail.ru>
2024-08-06 12:22:54 +0000

    bug-fix

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,5 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -20,5 +20,6 @@ struct binary_tree_s
 typedef struct binary_tree_s binary_tree_t;
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value); //task 2 
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value); //task 2
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 #endif
