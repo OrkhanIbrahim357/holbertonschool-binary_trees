4a2390852792458e481a5dea46866179ead5476d
Metin100 <9455@holbertonstudents.com>
2024-08-06 18:05:14 +0000

    solving task and betty problems

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_uncle - a function that finds the uncle of a node.
+ * @node: node.
+ * Return: uncle of node.
+ *
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *p;
+	binary_tree_t *c;
+
+	if (!node)
+		return (NULL);
+	p = node->parent->parent;
+	c = node->parent;
+	if (!p || !c)
+		return (NULL);
+	if (p->right == c && p->left)
+	{
+	return (p->left);
+	}
+	else if (p->left == c && p->right)
+	{
+		return (p->right);
+	}
+	return (NULL);
+
+}
