0765f741367d9829917dd3c3cecb848325e6c0c0
Metin100 <9455@holbertonstudents.com>
2024-08-07 12:10:49 +0000

    solving compile problem

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,34 +1,35 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance -  function measures balance factor of a binary tree.
- * @tree: tree.
- * Return: balance of binary tree.
- *
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: a pointer to the root node of tree
+ * Return: balance
  */
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	size_t left_h = 0, right_h = 0;
+
 	if (!tree)
 		return (0);
-
-	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	left_h = binary_tree_height(tree->left);
+	right_h = binary_tree_height(tree->right);
+	return (left_h - right_h);
 }
-
 /**
- * binary_tree_height - height of binary tree
- * @tree: tree.
- * Return: height of binary.
- *
+ * binary_tree_height - function that measures the height of a binary tree
+ * @tree: pointer to the root node of tree
+ * Return: 0
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (!tree)
+	size_t leftcount = 0, rightcount = 0;
+
+	if (tree == NULL)
 		return (0);
+	leftcount = leftcount + binary_tree_height(tree->left);
+	rightcount = rightcount + binary_tree_height(tree->right);
 
-	size_t l = binary_tree_height(tree->left);
-	size_t r = binary_tree_height(tree->right);
-
-	return ((l > r) ? l + 1 : r + 1);
+	return ((leftcount > rightcount) ? (leftcount + 1): (rightcount + 1));
 }
